4bfb5844813ff8f9530cb13cd8096946cec09212
Anderaxarex <a.scarpanti06@gmail.com>
2022-03-07 14:10:28 +0100

    commit #02

--- src/game.cpp
+++ src/game.cpp
@@ -14,6 +14,26 @@ void Game::run()
     init("Asteroids!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_screenheight, m_screenwidth, SDL_WINDOW_SHOWN);
     gameloop();
 }
+
+void Game::draw(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+    SDL_RenderClear(renderer);
+
+    SDL_Rect rect;
+    rect.x = 512;
+    rect.y = 300;
+    rect.w = 45;
+    rect.h = 60;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawRect(renderer, &rect);
+
+    SDL_SetRenderDrawColor(renderer, r, g, b, a);
+    SDL_RenderPresent(renderer);
+
+}
+
+
 void Game::init(const char* title, int x, int y, int w, int h, Uint32 flags)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -41,6 +61,10 @@ void Game::eventhandler()
         case SDL_QUIT:
             Game::m_gameState::QUIT;
             break;
+        default:
+            draw(0, 0, 0, 255);
     }
+
+
 
 }
